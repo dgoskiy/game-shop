556ab20ae62df4f23532e8054a4cfccf4895ae4c
urtblw <dgoskiy@gmail.com>
2024-03-30 17:30:14 +0300

    1.1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// 1.1
 root.render(
     <Provider store={store}>
       <App />
