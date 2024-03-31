eb6b239272e2102c0b6a113fccae2a2067876587
urtblw <dgoskiy@gmail.com>
2024-04-01 00:44:37 +0700

    1.8

--- src/components/Games/index.jsx
+++ src/components/Games/index.jsx
@@ -6,7 +6,7 @@ import { setLimitView } from '../../redux/store/filterSlice'
 import { useInView } from 'react-intersection-observer';
 const Games = (props) => {
   const { ref, inView } = useInView({
-    treshold: 0.1,
+    treshold: 0.5,
   })
   const dispatch = useDispatch()
 
@@ -14,19 +14,23 @@ const Games = (props) => {
     inView && dispatch(setLimitView(10))
   }, [inView])
   return (
-    <div className={s.games_container}>
-      {props.data.map(item => (
-        <div ref={ref} className={s.games_item} key={item.id}>
-          <div className={s.games_item_img}
-            style={{ backgroundImage: `url(${item.img})` }}>
+    <div className='w-100'>
+      <input type="text" />
+
+      <div className={s.games_container}>
+        {props.data.map(item => (
+          <div ref={ref} className={s.games_item} key={item.id}>
+            <div className={s.games_item_img}
+              style={{ backgroundImage: `url(${item.img})` }}>
+            </div>
+            <div className={s.body}>
+              <div className={s.forplay}>{item.forplay}</div>
+              <div className={s.name}>{item.name}</div>
+              <div className={s.cost}>{item.cost} ₴</div>
+            </div>
           </div>
-          <div className={s.body}>
-            <div className={s.forplay}>{item.forplay}</div>
-            <div className={s.name}>{item.name}</div>
-            <div className={s.cost}>{item.cost} ₴</div>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
