386f6bbed8893c9e5f25a176014b50b3699443b3
urtblw <dgoskiy@gmail.com>
2024-04-02 02:26:57 +0700

    2.2

--- src/App.js
+++ src/App.js
@@ -10,9 +10,17 @@ import { useNavigate } from 'react-router-dom'
 
 
 function App() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const filter = useSelector((state) => state.filter)
   const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    if (window.location.search) {
+      const params = ((qs.parse(window.location.search.substring(1))))
+      console.log(params)
+    }
+  }, [filter])
+
 
   React.useEffect(() => {
     axios.get(`https://65eb6f1043ce16418933d917.mockapi.io/api/game_shop/game?sortBy=${filter.sortProperty !== '' ? filter.sortProperty : ""}&order=${filter.order !== '' ? filter.order : ""}${filter.gcomp !== "" ? "&gameCompany=" + filter.gcomp : ""}${filter.gplat.length !== 0 ? "&gamePlatform=" + filter.gplat.join("|") : ""}${filter.search !== "" ? "&name=" + filter.search : ""}&page=1&limit=${filter.limitView}`)
@@ -30,8 +38,9 @@ function App() {
       page: 1,
       limit: filter.limitView
     })
-    console.log(queryString)
-    // navigate(`?${queryString}`)
+    // console.log(queryString)
+    navigate(`?${queryString}`)
+
   }, [filter])
 
   return (
